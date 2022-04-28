91e0beb5fd0e667c2f62cb10c1a8dd6192848040
Christiaan Bloemendaal <github@tnrd.net>
2022-04-28 22:10:54 +0200

    feat: added CustomObjectDrawer

---
+++ Editor/Drawers/CustomObjectDrawer.Styles.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TNRD.Drawers
+{
+    public partial class CustomObjectDrawer
+    {
+        private static class Styles
+        {
+            private static GUIStyle regularLabelStyle;
+            private static GUIStyle selectedLabelStyle;
+
+            public static GUIStyle RegularLabelStyle => regularLabelStyle ??= new GUIStyle(EditorStyles.label);
+
+            public static GUIStyle SelectedLabelStyle
+            {
+                get
+                {
+                    if (selectedLabelStyle == null)
+                    {
+                        selectedLabelStyle = new GUIStyle(EditorStyles.label)
+                        {
+                            normal =
+                            {
+                                textColor = EditorGUIUtility.isProSkin
+                                    ? new Color32(128, 179, 253, 255)
+                                    : new Color32(18, 73, 142, 255)
+                            },
+                            hover =
+                            {
+                                textColor = EditorGUIUtility.isProSkin
+                                    ? new Color32(128, 179, 253, 255)
+                                    : new Color32(18, 73, 142, 255)
+                            }
+                        };
+                    }
+
+                    return selectedLabelStyle;
+                }
+            }
+        }
+    }
+}
