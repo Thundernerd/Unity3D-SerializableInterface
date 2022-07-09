02fb8a98409afd296f1e430d6dc1988352e5262b
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2022-07-09 14:59:12 -0400

    Update SerializableInterfacePropertyDrawer.cs
    This fixes the issue #19

--- Editor/SerializableInterfacePropertyDrawer.cs
+++ Editor/SerializableInterfacePropertyDrawer.cs
@@ -10,7 +10,11 @@ namespace TNRD
     [CustomPropertyDrawer(typeof(SerializableInterface<>), true)]
     internal sealed class SerializableInterfacePropertyDrawer : PropertyDrawer
     {
+        private const string k_RawReference = "rawReference";
+
         private SerializedProperty serializedProperty;
+        private object previousReferenceValue;
+        private string lastPropertyPath;
         private Type genericType;
 
         private IReferenceDrawer activeDrawer;
@@ -23,14 +27,46 @@ namespace TNRD
 
         private void Initialize(SerializedProperty property)
         {
-            if (serializedProperty == property)
-                return;
-
-            activeDrawer = null;
             serializedProperty = property;
+            activeDrawer = null;
             genericType = GetGenericArgument();
+
             Assert.IsNotNull(genericType, "Unable to find generic argument, are you doing some shady inheritance?");
         }
+
+        /// <summary>
+        /// Avoids an unexpected behaviour
+        /// </summary>
+        private void AvoidDuplicateReferences(SerializedProperty property)
+        {
+            if (!IsTargetObjectArray(property)) return; // This function 
+            if (lastPropertyPath == null) return;
+            if (lastPropertyPath == property.propertyPath) return; // only one element
+
+            var rawReferenceProperty = property.FindPropertyRelative(k_RawReference); // Cache property
+            var currentReferenceValue = rawReferenceProperty.managedReferenceValue;
+
+            if (currentReferenceValue == null) return; // Value is null. Probably new or not set yet
+
+            if (previousReferenceValue == currentReferenceValue)
+            {
+                // The best behaviour would be to create a shallow copy. The extension method from SolidUtilities works perfectly.
+                // Using serialization or reflection might also work
+                var instance = Activator.CreateInstance(currentReferenceValue.GetType());
+                rawReferenceProperty.managedReferenceValue = instance;
+
+                // propertyRelative.managedReferenceValue = currentReferenceValue.ShallowCopy();
+            }
+
+            previousReferenceValue = currentReferenceValue;
+        }
+
+
+        private static bool IsTargetObjectArray(SerializedProperty prop)
+        {
+            return prop.propertyPath.Contains(".Array.data[");
+        }
+
 
         /// <inheritdoc />
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
@@ -44,8 +80,12 @@ namespace TNRD
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             Initialize(property);
+            AvoidDuplicateReferences(property);
+
             activeDrawer = GetReferenceDrawer(activeDrawer, property, label);
             activeDrawer.OnGUI(position);
+
+            lastPropertyPath = property.propertyPath;
         }
 
         private Type GetGenericArgument()
