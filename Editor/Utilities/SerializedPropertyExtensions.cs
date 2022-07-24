547c89271c892ca1bfa14e30958437c379dc9853
Christiaan Bloemendaal <github@tnrd.net>
2022-07-24 17:12:33 +0200

    feat: added serialized property extensions for convenience

---
+++ Editor/Utilities/SerializedPropertyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace TNRD.Utilities
+{
+    internal static class SerializedPropertyExtensions
+    {
+        public static SerializedProperty ReferenceModeProperty(this SerializedProperty property)
+        {
+            return property.FindPropertyRelative("mode");
+        }
+
+        public static SerializedProperty RawReferenceProperty(this SerializedProperty property)
+        {
+            return property.FindPropertyRelative("rawReference");
+        }
+
+        public static SerializedProperty UnityReferenceProperty(this SerializedProperty property)
+        {
+            return property.FindPropertyRelative("unityReference");
+        }
+    }
+}
