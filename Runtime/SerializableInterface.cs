7def5364163f45d9bcb568e14070cc50e3759203
Elias Hodel <elias.hodel@iart.ch>
2023-10-05 11:51:30 +0200

    Add Constructor with value

--- Runtime/SerializableInterface.cs
+++ Runtime/SerializableInterface.cs
@@ -15,6 +15,16 @@ namespace TNRD
         [HideInInspector, SerializeField] private UnityEngine.Object unityReference;
         [SerializeReference, UsedImplicitly] private object rawReference;
 
+        public SerializableInterface()
+        {
+
+        }
+        public SerializableInterface(TInterface value)
+        {
+            Value = value;
+        }
+
+
         public TInterface Value
         {
             get
