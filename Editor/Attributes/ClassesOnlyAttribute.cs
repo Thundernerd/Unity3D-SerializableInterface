873d16583c42b4c0934c83bc5313c80f22dec5a6
lordubbe <jacoblynggaardolsen@hotmail.com>
2023-11-08 10:56:58 +0100

    Added a bit of xml documentation

--- Editor/Attributes/ClassesOnlyAttribute.cs
+++ Editor/Attributes/ClassesOnlyAttribute.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 namespace TNRD
 {
+    /// <summary>An attribute to use when you want to exclude non-raw references in the reference picker.</summary>
     public class ClassesOnlyAttribute : PropertyAttribute { }
 }
