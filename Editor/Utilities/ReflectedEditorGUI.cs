f6a02aff7b84b3784441ff75d5ddb9749e2d761e
Christiaan Bloemendaal <github@tnrd.net>
2024-03-16 16:06:38 +0100

    add ReflectedEditorGUI

---
+++ Editor/Utilities/ReflectedEditorGUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace TNRD.Utilities
+{
+    public static class ReflectedEditorGUI
+    {
+        private static readonly PropertyInfo indentLevelProperty =
+            typeof(EditorGUI).GetProperty("indent", BindingFlags.Static | BindingFlags.NonPublic);
+        
+        private static readonly FieldInfo indentWidthField =
+            typeof(EditorGUI).GetField("kIndentPerLevel", BindingFlags.Static | BindingFlags.NonPublic);
+
+        static ReflectedEditorGUI()
+        {
+            indentWidth = (float)indentWidthField.GetValue(null);
+        }
+        
+        public static float indent => (float)indentLevelProperty.GetValue(null);
+
+        public static float indentWidth { get; private set; }
+    }
+}
